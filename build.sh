9b8548ec563c4c89587ef85cb8e04005a9d8af8b
AliSoAm <ali.sorour.amini@gmail.com>
2019-06-21 21:50:54 +0430

    add build.sh and fix ci/cd to use it

---
+++ build.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+UPDATE_DATE=$(date +'%Y-%m-%d %H:%M:%S')
+GIT_COMMIT_DATE=$(git log --format=%cd --date=short -1)
+GIT_COMMIT_HASH=$(git log --format=%h -1)
+
+echo "git commit date: ${GIT_COMMIT_DATE}"
+echo "git commit hash: ${GIT_COMMIT_HASH}"
+
+for dir in *; do
+  if [[ -d $dir ]] && [[ "$dir" != "public" ]]; then
+    echo "Entering ${dir}"
+    cd ${dir}
+    sed -i "s/Hash: NA/Hash: ${GIT_COMMIT_HASH}/g" ali_sorouramini.tex
+    sed -i "s/Updated at: NA/Updated at: ${GIT_COMMIT_DATE}/g" ali_sorouramini.tex
+    docker run --rm -v $(pwd):/tex aergus/latex /bin/sh -c "cd /tex; latexmk -pdf ali_sorouramini.tex"
+    mkdir -p ../public/${dir}
+    cp ali_sorouramini.pdf ../public/${dir}
+    echo "Leaving ${dir}"
+    cd ..
+  fi
+done
